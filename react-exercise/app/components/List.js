a1da7041a4475307a164bcf8061e18b4fd38dcbc
je-cg <jecg03@hotmail.com>
2016-06-09 17:21:06 -0700

    created List component

---
+++ react-exercise/app/components/List.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Card from './Card';
+
+class List extends Component {
+	render(){
+		var cards= this.props.cards.map((card) => {
+			return <Card id={card.id} title={card.title} description={card.description} task={card.task} />
+		});
+		return (
+			<div className="list">
+				<h1>{this.prop.title}</h1>
+				{cards}
+			</div>
+			);
+	}
+}
+
+export default List;
