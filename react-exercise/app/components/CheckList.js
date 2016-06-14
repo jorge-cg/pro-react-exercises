ff732ac306e7b9966d06e83219baa9c131b07134
je-cg <jecg03@hotmail.com>
2016-06-14 10:39:00 -0700

    added keys to the CheckList component

--- react-exercise/app/components/CheckList.js
+++ react-exercise/app/components/CheckList.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class CheckList extends Component {
 	render() {
 		let tasks= this.props.tasks.map((task) => (
-			<li className="checklist_task">
+			<li key={task.id} className="checklist_task">
 				<input type="checkbox" defaultChecked={task.done} />
 				{task.name}
 				<a href="#" className="checklist_task--remove" />
